a4c4118c51a3f9c38bc5376a1821f8c78fc9067c
Shubham Gupta <shubham1908gupta@gmail.com>
2023-03-22 22:01:07 +0530

    Time: 472 ms (75.85%), Space: 114.6 MB (91.39%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public: 
+    class DisjSet {
+    public:
+        vector<int> rank;
+        vector<pair<int, int>> parent;
+        int n;
+        
+        DisjSet(int n){
+            this->n = n;
+            this->rank = vector<int>(n+1);
+            this->parent = vector<pair<int, int>>(n+1);
+            for(int i=0;i<=n;i++){
+                parent[i] = {i, INT_MAX};
+            }
+        }
+        
+        pair<int, int> find(int u, int score){
+            if(parent[u].first != u){
+                parent[u] = find(parent[u].first, min(score, parent[u].second));
+            }
+            parent[u].second = min(score, parent[u].second);
+            // cout<<u<<" "<<score<<'\n';
+            return parent[u];
+        }
+        
+        void unionSet(int u, int v, int score){
+            // cout<<"insert "<<u<<" "<<v<<" "<<score<<"\n";
+            pair<int, int> a = find(u, score);
+            pair<int, int> b = find(v, score);
+            if(a.second < b.second){
+                parent[b.first] = a;
+            } else {
+                parent[a.first] = b;
+            }
+            // for(int i=1;i<=n;i++){
+            //     cout<<"parent "<<parent[i].first<<" "<<parent[i].second<<
+            //         '\n';
+            // }
+            // cout<<'\n';
+        }
+    };
+    
+    int minScore(int n, vector<vector<int>>& roads) {
+        DisjSet disS(n);
+        for(auto road: roads){
+            disS.unionSet(road[0], road[1], road[2]);
+        }
+        return disS.find(1, INT_MAX).second;
+    }
+};

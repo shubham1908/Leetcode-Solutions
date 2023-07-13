91ac2cf2ed964f14f55b6b2cacc43a412377f329
Shubham Gupta <shubham1908gupta@gmail.com>
2023-07-13 22:29:46 +0530

    Time: 16 ms (35.92%), Space: 6.3 MB (75.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.go
+++ 0207-course-schedule/0207-course-schedule.go
@@ -1,54 +1,28 @@
 func canFinish(numCourses int, prerequisites [][]int) bool {
-    pre := make([][]int, numCourses)
-    
+    adj := make([][]int, numCourses)
+    inDegree := make([]int, numCourses)
     for _, preReq := range prerequisites {
-        pre[preReq[0]] = append(pre[preReq[0]], preReq[1])
+        adj[preReq[1]] = append(adj[preReq[1]], preReq[0])
+        inDegree[preReq[0]]++
     }
     
-    level := make([]int, numCourses)
-    
-    // -numCourses-1 -> represents haven't picked yet
-    // numCourses+1  -> want to pick but level not defined yet
+    queue := []int{}
     for i:=0;i<numCourses;i++ {
-        level[i] = -numCourses-1
+        if inDegree[i] == 0 {
+            queue = append(queue, i)
+        }
     }
-    for i:=0;i<numCourses; i++{
-        if level[i] == -numCourses-1 {
-            if !helper(i, pre, level, numCourses){
-                return false
+    cnt := 0
+    for len(queue) != 0 {
+        course := queue[0]
+        queue = queue[1:]
+        cnt++
+        for _, c := range adj[course] {
+            inDegree[c]--
+            if inDegree[c] == 0{
+                queue = append(queue, c)
             }
         }
     }
-    return true
-}
-
-func helper(course int, pre [][]int, level []int, numCourses int) bool {
-    if len(pre[course]) == 0 {
-        level[course] = 0
-        return true
-    }
-    if level[course] == numCourses+1 { // already want to pick (cycle)
-        return false
-    }
-    level[course] = numCourses+1
-    lvl := -numCourses-1
-    for _, preCourse := range pre[course] {
-        if level[preCourse] == numCourses+1 {
-            return false
-        }
-        if level[preCourse] == -numCourses-1 {
-            if !helper(preCourse, pre, level, numCourses){
-                return false
-            }
-            if lvl < level[preCourse] +1 {
-                lvl= level[preCourse] +1
-            }
-        } else {
-            if lvl < level[preCourse] +1 {
-                lvl= level[preCourse] +1
-            }
-        }
-    }
-    level[course] = lvl
-    return true
+    return cnt == numCourses
 }

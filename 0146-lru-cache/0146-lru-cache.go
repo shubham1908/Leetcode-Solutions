2da700bed12383432caaf5708db9c888f2399277
Shubham Gupta <shubham1908gupta@gmail.com>
2023-07-18 23:02:37 +0530

    Time: 612 ms (37.23%), Space: 75.9 MB (52.34%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.go
@@ -0,0 +1,71 @@
+type LRUCache struct {
+	Head, Tail *Node
+	Mp map[int]*Node
+	Capacity int
+}
+
+func newLRUCache(head, tail *Node, cap int) LRUCache {
+	return LRUCache{
+		Head: head,
+		Tail: tail,
+		Mp: make(map[int]*Node),
+		Capacity: cap,
+	}
+}
+
+type Node struct {
+	Prev, Next *Node
+	Key, Value int
+}
+
+func Constructor(capacity int) LRUCache {
+    head := &Node{Key: 0, Value: 0}
+    tail := &Node{Key: 0, Value: 0}
+	head.Next = tail
+	tail.Prev = head
+	return newLRUCache(head, tail, capacity)
+}
+
+func (this *LRUCache) Get(key int) int {
+	if n, ok := this.Mp[key]; ok {
+		this.remove(n)
+		this.insert(n)
+		return n.Value
+	}
+
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if _, ok := this.Mp[key]; ok {
+		this.remove(this.Mp[key])
+	}
+
+	if len(this.Mp) == this.Capacity {
+		this.remove(this.Tail.Prev)
+	}
+
+	this.insert(&Node{Key: key, Value: value})
+}
+
+func (this *LRUCache) remove(node *Node) {
+	delete(this.Mp, node.Key)
+	node.Prev.Next = node.Next
+	node.Next.Prev = node.Prev
+}
+
+func (this *LRUCache) insert(node *Node) {
+	this.Mp[node.Key] = node
+	next := this.Head.Next
+	this.Head.Next = node
+	node.Prev = this.Head
+	next.Prev = node
+	node.Next = next
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */

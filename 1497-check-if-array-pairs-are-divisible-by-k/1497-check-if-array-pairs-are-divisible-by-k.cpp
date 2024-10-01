f02cb7267414d75428847b80bd15398b27a6d356
Shubham Gupta <shubham1908gupta@gmail.com>
2024-10-01 20:53:08 +0530

    Time: 80 ms (88.49%), Space: 65.4 MB (75.40%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         vector<long> div(k, 0);
-        int m = *min_element(arr.begin(), arr.end());
-        if(m < 0){
-            m=-m;
-        }
+
         for(auto e: arr){
-            int x = (1L* m*k + e)%k;
-            div[x]++;
+            div[(e%k+k)%k]++;
         }
         for(int i=1;i<k;i++){
             if(div[i] != div[k-i]){

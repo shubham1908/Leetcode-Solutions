2d300b74bda428de44e8ab348eaf746a145aea56
Shubham Gupta <shubham1908gupta@gmail.com>
2024-10-15 00:04:36 +0530

    Time: 754 ms (25.63%), Space: 31.3 MB (20.57%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
@@ -4,13 +4,13 @@ from math import ceil
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
         pq = []
-        heapq.heapify(pq)
+
         for item in nums:
-            heapq.heappush(pq, item *-1)
+            heapq.heappush(pq, -item)
          
         ans = 0
         for _ in range(k):
             x = -1* heapq.heappop(pq)
             ans += x
-            heapq.heappush(pq, ceil(x/3) * -1)
+            heapq.heappush(pq, -ceil(x/3))
         return ans
